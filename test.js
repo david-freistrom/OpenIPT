690a2f13e75a6618ee41f769e917bfc105a49d54
spautz <spautz@web.de>
2015-02-05 21:38:57 +0000

    correct and complete error messages for messages and helper

--- test.js
+++
@@ -1,5 +0,0 @@
-var IptBuffer = require('./lib/IptBuffer');
-
-var buffer = new Buffer("0102030405060708090001020304050607080900010203040506070809000001", 'hex');
-
-console.log(buffer.toString('hex'));
